63f218b4045f3eb6904864a6bd5034685bb3610e
agustik <agust.ingi.kjartansson@gmail.com>
2013-12-27 19:01:59 +0000

    .

--- frame.php
+++ frame.php
@@ -16,10 +16,10 @@
     <link href="./assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
   </head>
   <body>
-    <div class="col-md-9 video-frame">
+    <div class="col-md-7 video-frame">
               <iframe width="560" src="https://www.youtube.com/embed/lh6SBW4uGTY" frameborder="0" allowfullscreen></iframe>
             </div>
-            <div class="col-md-3 video-comments">
+            <div class="col-md-4 video-comments">
               <div class="fb-comments" data-href="https://edge.is" data-numposts="5" data-colorscheme="light" ></div>
             </div>
 
@@ -30,20 +30,33 @@
     <?php 
       $width = (array_key_exists('client-width', $_GET)) ? $_GET['client-width'] : 1400;
     ?>
-      var client = <?php print $width; ?> * 0.7;
+      var client = <?php print $width; ?> * 0.8;
       var elements = {
         video : $('.video-frame iframe'),
         comments : $('.video-comments .fb-comments')
       }
       var width = {
-          video   : client * 0.75,
-          comments: client * 0.25
+          video   : client * 0.7,
+          comments: client * 0.3
         }
 
       console.log(width, elements);
 
       elements.comments.attr('data-width', width.comments);
       elements.video.attr('width', width.video);
+      elements.video.attr('height', GetAspect(width.video, '4:3'));
+
+      function GetAspect(width, ratio){
+        var height;
+        if (ratio = "4:3"){
+          height = width * 0.8;
+        }
+        else if ( ratio = "16:9"){
+          height = width * 0.5425;
+        }
+        return height;
+      }
+
     </script>
     <div id="fb-root"></div>
 <script>(function(d, s, id) {
