54eac7a8370a74da59cdf5aa80f1a916f705f5b0
agustik <agust.ingi.kjartansson@gmail.com>
2013-12-29 01:51:35 +0000

    error reporting

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+	
+ error_reporting(true);
+
  include_once('config.php');
  $view = (array_key_exists('page', $_GET)) ? $_GET['page'] : "main.php";
  include_once DIR_BASE . 'models/' . $view;
