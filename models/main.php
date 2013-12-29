b7cf8e7327a9c37d2a32d029893afab1597f8e5a
agustik <agust.ingi.kjartansson@gmail.com>
2013-12-29 03:49:05 +0000

    javascript and cleanup

--- models/main.php
+++ models/main.php
@@ -192,55 +192,6 @@
     </footer>
     <!-- /Footer -->
 
-    <!-- Modal
-    <div id="marketing_modal" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-      <div class="modal-dialog-large">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h4 class="modal-title" id="myModalLabel">Haus</h4>
-          </div>
-          <div class="modal-body">
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-              <!-- Indicators 
-              <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-              </ol>
-
-              <!-- Wrapper for slides 
-              <div class="carousel-inner">
-          <div class="item active">
-            <iframe src="./frame.php" class="video-frame" scrolling="no"></iframe>
-          </div>
-          <div class="item">
-            
-          </div>
-          <div class="item">
-          </div>
-        </div>
-
-              <!-- Controls 
-              <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-              </a>
-              <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-              </a>
-            </div>
-
-            
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Loka</button>
-          </div>
-
-        </div><!-- /.modal-content 
-      </div><!-- /.modal-dialog 
-    </div>
-    <!--/Modal-->
-
     <!-- JavaScript -->
     <script src="./assets/js/jquery-1.10.2.js"></script>
     <script src="./assets/js/bootstrap.js"></script>
@@ -297,14 +248,10 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <script type="text/javascript">
-(function(element){
-  var iframe = $(element),
-      width = $(document).width(),
-      src = iframe.attr('src');
-      src = src + "?client-width=" + width;
-    iframe.attr('src', src);
-  }('.video-frame'));
 
+$(document).tooltip({
+  selector : '[data-toggle="tooltip"]'
+});
 </script>
 
   </body>
